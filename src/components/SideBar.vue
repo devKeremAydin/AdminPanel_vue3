d31805fda9e78bfdb63d53dbd684aaa0c878b6c8
kerem aydın <krmydn7@gmail.com>
2023-08-11 13:19:39 +0300

    feat/AdminPanel:Create panel

---
+++ src/components/SideBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-navigation-drawer v-show="true" v-model="localDrawer" app style="background-color: #24182f;">
+    <v-img src="">
+      <div class="text-center">
+        <v-avatar class="mt-4" size="64">
+          <v-img src="https://avatars.githubusercontent.com/u/64500329?v=4"></v-img>
+        </v-avatar>
+        <h3 style="color: white;">krmydn7@gmail.com</h3>
+      </div>
+    </v-img>
+
+    <v-divider></v-divider>
+
+    <v-list>
+      <v-list-item style="color: white;" v-for="[icon, text] in links" :key="icon" :prepend-icon="icon" :title="text" link></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  name: 'SideBar',
+  data() {
+    return {
+      links: [
+        ["mdi-microsoft-windows", "Dashboard"],
+        ["mdi-account", "Profile"],
+        ["mdi-clipboard-list-outline", "Products"],
+        ["mdi-card-account-details-outline", "Orders"],
+        ["mdi-cog", "System Setting"],
+      ]
+    }
+  }
+}
+</script>
+<style></style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar style="color: white; background-color: #24182f;" app :elevation="3">
+      <v-app-bar-nav-icon />
+      <v-toolbar-title>Application Bar</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon><v-icon>mdi-heart</v-icon></v-btn>
+      <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
+      <v-btn icon><v-icon>mdi-dots-vertical</v-icon></v-btn>
+    </v-app-bar>
+</template>
+<script>
+export default{
+    name:'TopBar'
+}
+</script>
+<style></style>
